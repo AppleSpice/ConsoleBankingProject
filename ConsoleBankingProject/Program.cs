f3ca466eda751aa87e7cb77d93bf141ec0ac5b0f
Gaven Rudiak <gavenpaul12@gmail.com>
2021-12-02 19:54:15 -0500

    Add project files.

---
+++ ConsoleBankingProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleBankingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
